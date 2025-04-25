c4456f68724dcfa13bfacb9f814f323f40c97a20
oldcookie <jxky2004@gmail.com>
2025-04-25 22:42:45 +0800

    q

--- local_send_plus/flutter_bootstrap.js
+++ local_send_plus/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2245182496"
+    serviceWorkerVersion: "3334122692"
   }
 });
